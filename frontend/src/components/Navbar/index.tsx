7260e30a52873c5fbe2ab5a1a50a5c7a33f694f7
Victor Trajano <trajanodev@hotmail.com>
2022-05-09 16:47:26 -0300

    Navbar

---
+++ frontend/src/components/Navbar/index.tsx
@@ -0,0 +1,20 @@
+import { ReactComponent as GithubIcon } from 'assets/img/github.svg'
+import './styles.css'
+
+export function Navbar() {
+  return (
+    <header>
+      <nav className="container">
+        <div className='dsmovie-nav-content'>
+          <h1 className='text-white font-weight-bold'>DSMovie</h1>
+          <a href="https://github.com/vtrajanodev">
+            <div className='d-flex text-white'>
+              <GithubIcon />
+              <p>/vtrajanodev</p>
+            </div>
+          </a>
+        </div>
+      </nav>
+    </header>
+  )
+}
